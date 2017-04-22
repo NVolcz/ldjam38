4031291e1a05e3377c2a48c4c6d786114e027f0a
Joel Björklund <joel.bjorklund@groplay.se>
2017-04-22 15:17:15 +0200

    test event system joel

---
+++ Assets/TestJ2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using UnityEngine.Events;
+
+
+public class TestJ2 : MonoBehaviour  {
+	public class MachineEvent : UnityEvent<bool> {}
+
+	public MachineEvent Machin = new MachineEvent ();
+	public UnityEvent OnActive = new UnityEvent ();
+
+	public bool IsOn;
+
+	void OnEnable()
+	{
+		var count =
+			Machin.GetPersistentEventCount ()
+			+ OnActive.GetPersistentEventCount ();
+		if (count == 0)
+		{
+			Debug.Log ("No events atatched");
+		}
+	}
+
+
+
+	//public class MachineEvent : Unity
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (IsOn)
+		{
+			OnActive.Invoke ();
+		}
+		
+	}
+}
