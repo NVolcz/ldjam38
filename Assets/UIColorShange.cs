31abe62a0c50b87d93befdf1a3a590c722b6a523
Joel Björklund <joel.bjorklund@groplay.se>
2017-04-22 18:34:52 +0200

    Ui to Steameboilder

---
+++ Assets/UIColorShange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIColorShange : MonoBehaviour {
+
+	public GameObject target;
+	public Image image; 
+	private Slider slider;
+	// Use this for initialization
+	void Start () {
+		image = target.GetComponent<Image> ();
+		slider = GetComponent<Slider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var color = new Color (slider.value, 1 - slider.value, 1 - slider.value);
+		image.color = color;
+	}
+}
