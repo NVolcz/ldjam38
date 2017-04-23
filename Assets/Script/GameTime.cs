25984d170c5a6c1ec57581868661314fdab92c8e
Johan Rende <johan@rende.se>
2017-04-23 16:04:59 +0200

    Added game over scen

---
+++ Assets/Script/GameTime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameTime : MonoBehaviour {
+	public static DateTime startTime = DateTime.Now;
+	public static DateTime endTime = DateTime.Now;
+	public static double getTimeSurvived() {
+		return (endTime - startTime).TotalMilliseconds / 1000.0;
+	}
+}
---
+++ Assets/Script/SetTimeText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetTimeText : MonoBehaviour {
+	public GameObject gameObject;
+	private Text text;
+	// Use this for initialization
+	void Start () {
+		text = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string text2 = text.text;
+		text2 = text2.Replace("${NUMBER_OF_SECONDS}", GameTime.getTimeSurvived().ToString());
+		text.text = text2;
+	}
+}
