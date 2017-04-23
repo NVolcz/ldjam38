3d6d5ed010d56398cb33d9c21dc058c474908f01
Joel Björklund <joel.bjorklund@groplay.se>
2017-04-23 19:31:13 +0200

    energibar uppkoplad

---
+++ Assets/Script/UpdateEfficencyBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateEfficencyBar : MonoBehaviour {
+
+	public GameObject target; 
+	private SolarPanel panal; 
+
+	// Use this for initialization
+	void Start () {
+		panal = target.GetComponent<SolarPanel> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.GetComponent<Slider> ().value = (float)panal.efficiency;
+	}
+}
